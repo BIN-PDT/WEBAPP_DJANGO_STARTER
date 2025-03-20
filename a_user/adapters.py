4eb6962cee66eb2756c899863e214ebc11d465d5
BIN BIN <thangpd300502@gmail.com>
2025-03-20 22:11:27 +0700

    FIX SOCIAL ACCOUNT LOGIN LOGIC

--- a_user/adapters.py
+++ a_user/adapters.py
@@ -1,8 +1,9 @@
 from allauth.account.adapter import DefaultAccountAdapter
 from allauth.account.models import EmailAddress
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
-from django.contrib.auth.models import User
-from django.shortcuts import resolve_url
+from allauth.core.exceptions import ImmediateHttpResponse
+from django.contrib import messages
+from django.shortcuts import resolve_url, redirect
 
 
 class CustomAccountAdapter(DefaultAccountAdapter):
@@ -10,33 +11,10 @@ class CustomAccountAdapter(DefaultAccountAdapter):
         return resolve_url("profile-onboarding")
 
 
-class SocialAccountAdapter(DefaultSocialAccountAdapter):
+class CustomSocialAccountAdapter(DefaultSocialAccountAdapter):
     def pre_social_login(self, request, sociallogin):
-        email = sociallogin.account.extra_data.get("email")
-
-        if not email:
-            return None
-
-        if sociallogin.is_existing:
-            user = sociallogin.user
-            email_address, _ = EmailAddress.objects.get_or_create(
-                user=user, email=email
-            )
-            if not email_address.verified:
-                email_address.verified = True
-                email_address.save()
-        else:
-            existing_user = User.objects.filter(email=email).first()
-            if existing_user:
-                sociallogin.connect(request, existing_user)
-
-    def save_user(self, request, sociallogin, form=None):
-        user = super().save_user(request, sociallogin, form)
-        email_address, _ = EmailAddress.objects.get_or_create(
-            user=user, email=user.email
-        )
-        if not email_address.verified:
-            email_address.verified = True
-            email_address.save()
-
-        return user
+        if not sociallogin.is_existing:
+            email_addresses = [e.email for e in sociallogin.email_addresses]
+            if EmailAddress.objects.filter(email__in=email_addresses).exists():
+                messages.error(request, "Email is already linked to another account.")
+                raise ImmediateHttpResponse(redirect("account_login"))
